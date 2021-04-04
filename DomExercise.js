e0748f30bbea1d8e6624e5f6f3f8cbf5ac0721d0
Chaz Logan <chazaqlogan@gmail.com>
2021-04-03 21:33:12 -0400

    2nd Commit

--- DomExercise.js
+++ DomExercise.js
@@ -2,3 +2,32 @@ const sec = document.getElementById("container")
 const sec = document.querySelector("#container")
 document.getElementsByClassName("second") or document.querySelectorAll('.second')
 document.querySelector("ol .third")
+const sayHello = document.getElementsByClassName("container")
+sayHello.innerText = "Hello!"
+let footer = document.querySelector(".footer");
+footer.classList.add("main");
+or
+footer.className += "main";
+
+let footer = document.querySelector(".footer");
+footer.classList.remove("main");
+const newEle = document.createElement('li')
+newEle.innerText = "four";
+
+
+let list = document.querySelector("ul")
+list.appendChild(newEle)
+
+//loop over all of the lis inside the old tag and give them a background color of green
+let liIn = document.querySelectorAll("ol li");
+
+for (let i = 0; i < liIn.length; i++) {
+    liIn[i].getElementsByClassName.backgroundColor = "green";
+
+
+}
+
+
+// remove div with class of footer
+let footer = document.querySelector("footer");
+footer.remove();
